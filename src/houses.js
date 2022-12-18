55973f46940320f431059e74d4a0ad66bcf8c900
Dima-Ukhanov <shuffle18@rambler.ru>
2022-12-18 21:18:37 +0200

    adds HW #10

--- src/houses.js
+++ src/houses.js
@@ -1,22 +1,80 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const detailsContainer = document.querySelector(".details-container");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 const mainElement = document.querySelector(".main-class");
 const hideButtonElement = document.querySelector("#hide-button");
+const audio = document.querySelector("#audio");
+const logoPlay = document.querySelector(".logo");
+
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
+
+const playAudio = document.querySelector("#play");
+const pauseAudio = document.querySelector("#pause");
+
+playAudio.addEventListener("click",playDetails);
+pauseAudio.addEventListener("click",playsDetails);
+
+function playDetails(){
+    audio.play();
+
+}
+function playsDetails(){
+    audio.pause();
+}
+
+
+ 
+
+// window.onclick = function(){
+//     audio.play();
+// }
+
+function showAudio(){
+   audio.play();
+
+}
+function stopAudio(){
+    audio.pause();
+    audio.currentTime = 0;
+   
+ 
+ }
+//  function pauseAudio(){
+//     audio.pause();
+    
+//  }
+
+
+
 function showDetails(){
     mainElement.classList.remove(HIDDEN);
+    logoPlay.classList.remove(HIDDEN);
+
+    detailsContainer.classList.add(IS_POINT);
+    showAudio();
+    setTimeout(function(){
+        detailsContainer.classList.remove(IS_POINT);
+    })
+    
+
 }
 function hideDetails(){
     mainElement.classList.add(HIDDEN);
+    stopAudio();
+    logoPlay.classList.add(HIDDEN);
 
 }
+
 
 
 function setDetails(anchor){
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
     showDetails();
+  
+  
   
    
    
@@ -40,3 +98,5 @@ for(let i = 0; i < anchorElements.length; i++) {
 
 }
 hideButtonElement.addEventListener("click",hideDetails);
+
+

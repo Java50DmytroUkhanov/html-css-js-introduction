53169d6d071b323ac30b5c4deb20b0b1fceae737
Dima-Ukhanov <shuffle18@rambler.ru>
2023-01-05 18:42:18 +0200

    HW #14

--- app.js
+++ app.js
@@ -1,95 +1,19 @@
-// const ar = [];
-// const ar1 = [1,2,3,4,];
-// ar.push(...ar1);
-// ar.push('abc');
+//HW #14 (2)
+function getNumbersWithDigitsAmount(digitsAmount, array) {
+    //takes: digitAmount - given number of digits; array - any array of numbers
+    //returns array of only numbers from the given array comprising of the given number of digits
+    //examples: getNumbersWithDigitsAmount (3, [1, 100, -100, 25, 1000]) returns [100, - 100]
+    //
 
-function getRandomNumber (min, max){
-  min = Math.trunc(min);
-  max = Math.trunc(max);
-  return min + Math.trunc(Math.random() * (max - min + 1));
+    const getNumbers = array.filter(function(number){
+       let str = String(number).length;
+        if(String(number).indexOf('-') == 0 ){
+         str = str -1;
+        }
+        return str == digitsAmount;
+                              
+    })
+  return getNumbers;
 
-}
-
-function getRandomMatrix(rows, column, min, max){
-  const matrix = [];
-
-  for(let i = 0; i < rows; i++){
-      matrix.push([]);
-      for(let j = 0; j < column; j++ ){
-          matrix[i].push(getRandomNumber(min, max));
-      }
-  }
-   
-  
-  return matrix;
-}
-//const matrix = getRandomMatrix(3, 4, 0, 1);
-
-const ar10 = [1,2,3,4,5];
-const str = ar10.join('_');
-
-// HTML
-// <ul class ="list_class">
-//   <li class=" item_class">
-//        <div class="white"> </div>
-//   </li>
-//................
-//</ul>
-/* <ul class ="list_class">
-//   <li class=" item_class">
-//        <div class="white"> </div>
-//   </li>
-//................
-//</ul> */
-function getHTMLUL(array){
-    
-  let htmlString =  '<ul class ="list_class">';
-  let string = " ";
- for( let i = 0; i < array.length; i++){
-  const strClass = array[i] === 0 ? 'white' : 'black';
-  
-   string = '<li class=" item_class">' + `<div class="${strClass}"> </div>` + '</li>';
-    htmlString = htmlString +  string;
-     
-  }
-  htmlString = htmlString + '</ul>';
-
- return htmlString;
-
-}
-
-function getArray(min, max, size){
-
-   const array = new Array(size);
-  for( let i = 0; i < array.length; i++){
-      array[i] = getRandomNumber (min, max);
-       
-  }
-  return array;
-}
-console.log( getHTMLUL(getArray(0, 1, 10)));
-
-
-const strClass = getRandomNumber(0, 1) === 0 ? 'white' : 'black'
-const str1 = "hello word";
-const str2 = 'hello' + '"word"';
-const str3 = `class ="${strClass}"`;                    // может форматировать ( тильда)
-
-function matrixTransp (matrix){
- 
-  let m = matrix.length, n = matrix[0].length, column = [];
-  for (let j = 0; j < n; j++){
-    column[j] = [];
-
-     for (let i = 0; i < m; i++) {
-     column[j][i] = matrix[i][j];
-   }
-
-}
-return column;
-
-}
-
-console.log(matrixTransp (getRandomMatrix(4, 2, 0, 1)));
-console.log(matrixTransp (getRandomMatrix(2, 4, 0, 1)));
-
+ }
+console.log(getNumbersWithDigitsAmount(4, [1, 100, -100, 25, 1000]));

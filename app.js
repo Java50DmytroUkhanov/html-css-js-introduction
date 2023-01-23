132e94b3e13711592e9c5c904aaa5e220806608c
Dima-Ukhanov <shuffle18@rambler.ru>
2023-01-23 19:50:20 +0200

    adds HW #19

--- app.js
+++ app.js
@@ -1,97 +1,125 @@
-const employee1 = {id: 123, name: "Vasya", birthYear: 2000,salary: 15000, address: {city: "Lod", country: "Israel"}};
-const employee2 = {id: 123, name: "Vasya", birthYear: 2000,salary: 15000, address: {city: "Lod", country: "Israel"}};
-console.log(`employee1 == employee2 is ${employee1 == employee2}` );
-const employee3 = employee1; 
-console.log(`employee3 == employee1 is ${employee3 == employee1}` );
-employee3.salary = 20000;
-console.log(`employee1 salary = ${employee1.salary}`);
-function createEmployee(id, name, bithYear, salary, city,country) {
-    return {id, name, bithYear,salary, address:{city, country} };
+let field = "salary";
+// console.log(employees[0][field]);
+
+function displayFieldValue(employees,index,field){
+    console.log(employees[index][field]);
 }
+// displayFieldValue(employees, 3, "bithYear") 
+// employees[0].salary = 20000;
+// employees[0].department = "QA";
+// displayFieldValue(employees, 0, "department");
+// delete employees[0].department
+// displayFieldValue(employees, 0, "department");
 
-const employees = [
-    createEmployee(123, "Vasya", 2000, 15000, "Lod", "Israel"),
-    createEmployee(124, "David", 1975, 15050, "Tel Aviv", "Israel"),
-    createEmployee(125, "Sara", 1985, 20000, "New York", "USA"),
-    createEmployee(126, "Abraham", 1990, 13000, "London", "UK"), 
-    createEmployee(127, "Moshe", 2000, 15000, "Rehovot", "Israel"),
-    createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "Gorgia"),
-    createEmployee(129, "Sash", 2000, 25000, "Ramat Gan", "Israel"),
-    createEmployee(130, "Victor", 2003, 10000, "Arad", "Israel")
-]
-//const index = employees.indexOf( createEmployee(126, "Abraham", 1990, 13000, "London", "UK")) // not right!!
-const index = employees.findIndex(function(empl){
-    return empl.id === 126;
-})
-const employee = employees.find(function(empl){
-    return empl.id === 126;
-})
+function computeMinMaxAvgSalary(employees){
+    const res = employees.reduce((res, empl) => {
+        if(res.minSalary > empl.salary){
+            res.minSalary = empl.salary;
+        } else if( res.maxSalary < empl.salary){
+            res.maxSalary = empl.salary;
+        }
+        res.avgSalary += empl.salary;
+        return res;
+        
+    }, {minSalary: employees[0].salary, maxSalary: employees[0].salary, avgSalary: 0});
+    res.avgSalary /= employees.length;
+    return res;
+}
+function displayValue(minMaxAvg, field){
+    console.log( `value of the field ${field} is ${minMaxAvg[field]}`)
 
-//HW # 18
-function getEmployee(employees, id) {
+};
+const minMaxAvg = computeMinMaxAvgSalary(employees);
+displayValue(minMaxAvg,"avgSalary");
+displayValue(minMaxAvg,"minSalary");
+displayValue(minMaxAvg,"maxSalary");
+
+const strings = ["b", "xyz", "lmn", "xyz", "lmn", "xyz", "a"];
+//assumed result:
+//xyz -> 3
+//lmn -> 2
+//a -> 1
+//b -> 1
+function displayStringOccurrences(strings) {
+    const stringOccurrences = getStringOccurrences(strings);
+    const arrayOccurrences = Object.entries(stringOccurrences);
+    arrayOccurrences.sort(stringComp);
+    arrayOccurrences.forEach(entry => console.log(`${entry[0]} -> ${entry[1]}`));
+
+}
+function getStringOccurrences(strings) {
+    const res = {};
+    strings.forEach(str => {
+        if(!res[str]) {
+            res[str] = 1;
+        } else {
+            res[str]++;
+        }
+    });
+    return res;
+}
+function stringComp(entry1, entry2) {
+    let res = entry2[1] - entry1[1];
+    if(res == 0){
+        res = entry1[0] < entry2[0] ? -1 : 1
+    }
+    return res;
+}
+displayStringOccurrences(strings);
+//HW #19
+function getMostPopulatedCountry(employees){
     //TODO
-    //returns reference to an Employee object with a given id value
-     const employee = employees.find(function(empl){
-        return empl.id === id;
-     })
-    return employee;
+    //returns country with most amount of employees
+    let res = '';
+    const objectCountry = getCountry(employees);
+    const arrayCountry = Object.entries(objectCountry);
+    arrayCountry.sort((a, b) => a[1] - b[1]);
+    let length = arrayCountry.length - 1;
+    res = arrayCountry[length][0];
+    return res;
+
 }
-console.log("Test 1", getEmployee(employees, 126));
+console.log(getMostPopulatedCountry(employees));
 
-
-function getEmployeesBySalary(employees, salaryFrom, salaryTo){
-   //TODO
-   // returns arrays of Employee object that have salary in [salaryFrom, salaryTo] 
-   const arrayEmployees = employees.filter(function(empl){
-       return empl.salary >= salaryFrom && empl.salary <= salaryTo;
-
-   })
-   return arrayEmployees;
+function getCountry(employees){
+    const array = {};
+    employees.forEach(empl => {
+      if(!array[empl.address.country]){
+        array [empl.address.country] = 1;
+      }else{
+        array [empl.address.country]++;
+      }
+      
+    });
+    return array;
 }
-console.log("Test 2",getEmployeesBySalary(employees, 13000, 15000));
+
+function getMostPopulatedCountries(employees, counter){
+    //return a given number (conter) of countries with most amount of employees
+    
 
 
-function getEmployeesByCity(employees,city){
+
+}
+function isAnagram(word, anagram) {
+  let res = false;
     //TODO
-    //returns array of Employee objects from a given city 
-   const cityEmployees = employees.filter(function(empl){
-    return empl.address.city === city;
-
-   })
-  return cityEmployees;
+    //returns true if a given anagram is indeed an angram of a given word
+    //anagram must have the same length as word
+    //anagram must have only the same letters as word
+    //hello anagram examples: elolh, olleh, ohell, lehol
+    //exampls non-anagrams: eloll, ...
+      if(anagram.length === word.length){
+        const str1 = anagram.split('');
+        const str2 = word.split('');
+        str1.sort();
+        str2.sort();
+        let st = str1.join('');
+        let st2 = str2.join('');
+       res = (st === st2);
+      }
+   
+   return res;
 }
-console.log("Test 3", getEmployeesByCity(employees,"Lod"));
 
-
-function getEmployeeNames(employees){
-    //TODO 
-    // returns array of all Employee names 
-     const nameEmployees = employees.map(function(empl){
-        return empl.name;
-     })
-     return nameEmployees;
-}
-console.log("Test 4",getEmployeeNames(employees));
-
-
-function sortEmployeesByAge(employees){
-    //TODO
-    //returns array of Employee objects sorted by age in ascending order
-    const ageEmployees = employees.sort(function(empl,empl2){
-        return empl2.bithYear - empl.bithYear;
-    })
-   return ageEmployees;
-
-}
-console.log("Test 5", sortEmployeesByAge(employees));
-
-
-function computeSalaryBudget(employees) {
-    //TODO 
-    //computes and returns total salary for all Employee objects
-  const totalSalary = employees.reduce(function( res, empl ){
-    return res += empl.salary;
-  }, 0)
-    return totalSalary;
-}
-console.log("Test 6",computeSalaryBudget(employees));
+console.log(isAnagram('elolh','olleh'));
